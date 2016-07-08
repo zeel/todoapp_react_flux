f3b0d1096cabcbbcb78cc89e4fddd9c72bf7f940
zeel shah <zeel.shah@sprinklr.com>
2016-07-08 11:43:03 +0530

    started with webpack hello world in react

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoApp from './components/ToDo';
+
+
+ReactDOM.render(<TodoApp />,
+    document.getElementById('todo-app'));
---
+++ src/js/components/ToDo.js
@@ -0,0 +1,6 @@
+/**
+ * Created by zeelshah on 08/07/16.
+ */
+import React from 'react';
+import 'css/style.css';
+export default () => <div>hello world!</div>
